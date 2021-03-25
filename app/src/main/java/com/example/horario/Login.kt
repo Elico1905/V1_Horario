9d127cc5d1250b657b6e48c7355adc7b2efe984d
Gonzalo Ballesteros <elico1904@hotmail.com>
2021-03-25 01:22:09 -0600

    initial commit

--- app/src/main/java/com/example/horario/Login.kt
+++ app/src/main/java/com/example/horario/Login.kt
@@ -1,19 +1,68 @@
 package com.example.horario
 
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+    private val GOOGLE_SING_IN = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.activity_login)
 
-        carta.setOnClickListener {
-            val intent: Intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        cardView_google.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SING_IN)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SING_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                if (account != null) {
+                    //MostrarAnimacion()
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            //showHome(account.email?:"",account.givenName?:"",account.familyName?:"",ProviderType.GOOGLE)
+                            //consultamos a firebase que el usuario este registrado
+                            //SharedCorreo = account.email ?: ""
+                            //SharedNombre = account.givenName ?: ""
+                            //SharedApellidos = account.familyName ?: ""
+                            //provedor = ProviderType.GOOGLE
+                            //GetDatosFirebase(account.email ?: "")
+                            val intent: Intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                            finish()
+
+                        } else {
+                            //error de que no hay cuenta seleccionada
+                        }
+                    }
+
+                }
+            } catch (e: ApiException) {
+            }
+
+        }
+    }
+
 }
